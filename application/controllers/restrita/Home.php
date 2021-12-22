54f989b44f701c70c137dca646bd1392d25f380a
Emerson Nascimento <tc.emerson.nascimento@gruponc.net.br>
2021-12-21 23:04:50 -0300

    Preparando o ambiente

---
+++ application/controllers/restrita/Home.php
@@ -0,0 +1,21 @@
+<?php
+
+defined('BASEPATH') OR exit('Ação não permitida');
+
+class Home extends CI_Controller{
+    
+    public function __construct() {
+        parent::__construct();
+        
+        //Existe uma sessão?
+    }
+    
+    public function index() {
+        
+        $this->load->view('restrita/layout/header');
+        $this->load->view('restrita/home/index');
+        $this->load->view('restrita/layout/footer');
+        
+    }
+}
+
